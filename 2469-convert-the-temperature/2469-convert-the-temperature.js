02d1e43a9c739e13e3822c771bc5fbb8e0ed1fdd
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2022-11-27 23:31:34 +0200

    Time: 62 ms (93.82%), Space: 42 MB (43.14%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function (celsius) {
+    let kelvin = celsius + 273.15;
+    let fahrenheit = celsius * 1.8 + 32.0;
+
+    return [kelvin, fahrenheit];
+};
