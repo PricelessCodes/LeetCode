b1e31b71a0c326748d3ad9f6a5489e28f103d3e6
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2022-12-07 00:30:12 +0200

    Time: 302 ms (60.84%), Space: 51.8 MB (13.80%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if (x < 0) return false;
+
+    if (x < 10) return true;
+
+    //convert x to string
+    x = x.toString();
+
+    let start = 0;
+    let end = x.length - 1;
+
+    while (start < end) {
+        if (x[start] !== x[end]) return false;
+
+        start++;
+        end--;
+    }
+
+    return true;
+};
