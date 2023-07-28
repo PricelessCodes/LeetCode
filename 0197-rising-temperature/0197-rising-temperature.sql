acac09dcf5c970da63222e20d3405ba9c11d1605
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2023-07-28 18:45:44 +0300

    Time: 1539 ms (7.90%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select w2.id
-from Weather as w1 join Weather as w2
-where w1.temperature < w2.temperature &&  TO_DAYS(w2.recordDate)-TO_DAYS(w1.recordDate)=1;
+/* Write your T-SQL query statement below */
+select distinct w2.id as "Id"
+from Weather as w1 join Weather as w2 on DATEDIFF(day, w1.recordDate, w2.recordDate) = 1
+where w1.temperature < w2.temperature
